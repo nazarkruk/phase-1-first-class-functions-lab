73a90e7536ebb1ed35351faccf9fc6f1bd8a3b16
Nazar Kruk <nazarkruk@icloud.com>
2022-04-29 21:30:19 -0400

    commit tree cc803b7be2b68f5d5a90dd9b594be9daed717b50

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(driversArray){
+    return driversArray.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(driversArray){
+    return driversArray.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(inputInt){
+    return fare => fare * inputInt
+}
+
+const fareDoubler = fare => fare * 2 
+
+const fareTripler = fare => fare * 3
+
+const selectDifferentDrivers = function (driversArray, fun){
+    
+    return fun(driversArray)
+}
